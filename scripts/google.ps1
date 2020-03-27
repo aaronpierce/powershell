f8ae3e7a5cfb7f223c3cb983cb2082d950e5760c
Aaron Pierce <aaronpierce15@gmail.com>
2020-03-27 16:36:50 -0500

    Added google function

---
+++ scripts/google.ps1
@@ -0,0 +1,29 @@
+function google {
+
+    [CmdletBinding()]
+    Param(
+    	[Parameter(Mandatory=$True,Position=0)]
+    	[String]$SearchFor,
+    	
+    	[Parameter(Mandatory=$False,Position=1)]
+    	[String]$Use = "Google"
+    )
+
+    $ErrorActionPreference = "SilentlyContinue"
+    If ($Error) {$Error.Clear()}
+
+    $SearchFor = $SearchFor.Trim()
+    If (!($SearchFor)) {
+    	Write-Host
+    	Write-Host "Text That You Wish To Search For Has Not Been Entered." -ForeGroundColor "Yellow"
+    	Write-Host "Execution of the Script Has been Ternimated." -ForeGroundColor "Yellow"
+    	Write-Host
+    	Exit
+    }
+
+    $SearchFor = $SearchFor -Replace "\s+", " "
+    $SearchFor = $SearchFor -Replace " ", "+"
+
+   	Start "https://www.google.com/search?q=$SearchFor"
+
+}
--- scripts/oneliners.ps1
+++ scripts/oneliners.ps1
@@ -2,6 +2,10 @@ function touch($file) { "" | Out-File $file -Encoding ASCII }
 
 function getip($hostname) { [Net.Dns]::GetHostByName($hostname).AddressList.IPAddressToString } 
 
+function gethostname($ip) { [system.net.dns]::GetHostByAddress($ip).hostname }
+
 function ext($file) { [IO.Path]::GetExtension($file) }
 
 function regex($text, $pattern) { [RegEx]::Matches($text, $pattern).Value}
+
+function here() { . explorer.exe }
